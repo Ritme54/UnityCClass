50e512a3ad796bea49273fdefffa79752c057186
Ritme54 <tmdwns5456@naver.com>
2025-09-02 15:41:05 +0900

    (2025.09.02)수출용 패키지

---
+++ Game/Assets/Class2th(Script)/Script/Ability.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Ability : MonoBehaviour
+{
+
+    public int strength = 5;
+    
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+       
+
+        Debug.Log("strength : "+ strength);
+    }
+
+    // Update is called once per frame
+   
+}
